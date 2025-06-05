9ddbcc5b8968c42f4a8c099074e2338a93f5a4ac
duridev <duridev@outlook.com>
2025-06-04 23:18:10 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mod3_ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int importe;
+            float totalPagar;
+
+            Console.WriteLine("Ingrese el importe de la compra:");
+            importe = int.Parse(Console.ReadLine());
+
+            if (importe > 5000)
+            {
+                totalPagar = 
+                Console.WriteLine("No hay descuento");
+            }
+        }
+    }
+}
